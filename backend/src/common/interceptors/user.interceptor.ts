7f385ddad39f4184b9f6ba6e1ea6539a0015a126
subin <github@subin.kr>
2023-12-13 09:16:07 +0900

    feat: add user interceptor

---
+++ backend/src/common/interceptors/user.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class UserInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+
+    return next.handle().pipe(map((data) => ({ user: req.user, data })));
+  }
+}
