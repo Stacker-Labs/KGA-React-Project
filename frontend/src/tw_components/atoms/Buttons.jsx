8a3feb298d601f173e81ba6f9252e9c9d0e65628
Eunjae Lee <vanitasium@gmail.com>
2023-12-13 14:56:40 +0900

    add: tailwind clsx, cva, twmerge setup

---
+++ frontend/src/tw_components/atoms/Buttons.jsx
@@ -0,0 +1,48 @@
+import { ButtonHTMLAttributes, Children, FC } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../../utils/cn";
+
+export const ButtonVariants = cva(
+  `
+    flex justify-center items-center active:scale-95 rounded-xl 
+    text-sm font-bold text-slate-100 transition-all shadow-md
+    hover:scale-105 duration-200
+    `,
+  {
+    variants: {
+      variant: {
+        default: " shadow-none active:scale-100",
+        grey: " bg-slate-buttongrey ",
+        blue: " bg-accent-blue",
+      },
+      size: {
+        default: "",
+        md: " w-[6.875rem] h-[2.375rem] text-[1rem] rounded-md",
+        lg: "w-[21.875rem] h-[7.5rem] text-[3rem] rounded-3xl",
+        wlg: "w-[24rem] h-[5.25rem] text-[2rem]",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+// interface ButtonProps
+//   extends ButtonHTMLAttributes<HTMLButtonElement>,
+//     VariantProps<typeof ButtonVariants> {
+//   label?: string;
+//   children?: React.ReactElement;
+// }
+
+const Button = ({ variant, size, children, label, ...props }) => {
+  return (
+    <button className={cn(ButtonVariants({ variant, size }))} {...props}>
+      {children && children}
+      {label && label}
+    </button>
+  );
+};
+
+export default Button;
