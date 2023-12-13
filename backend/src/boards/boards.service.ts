91ae77bbc48640f731ebc7dc1c8ed6698f428eb2
subin <github@subin.kr>
2023-12-13 09:14:54 +0900

    feat: Inject dependency

--- backend/src/boards/boards.service.ts
+++ backend/src/boards/boards.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardModel } from './entities/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardsService {
+  constructor(
+    @InjectRepository(BoardModel)
+    private readonly boardRepository: Repository<BoardModel>,
+  ) {}
+
   create(createBoardDto: CreateBoardDto) {
     return 'This action adds a new board';
   }
