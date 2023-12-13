d7b93b1a139259cc55a2365d00ceaedf56c0dacd
subin <github@subin.kr>
2023-12-13 14:21:41 +0900

    feat: remove function

--- backend/src/boards/boards.service.ts
+++ backend/src/boards/boards.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardModel } from './entities/board.entity';
 import { Repository } from 'typeorm';
+import { Role } from 'src/common/const/role.enum';
 
 @Injectable()
 export class BoardsService {
@@ -12,19 +17,40 @@ export class BoardsService {
     private readonly boardRepository: Repository<BoardModel>,
   ) {}
 
-  create(createBoardDto: CreateBoardDto) {
+  async create(createBoardDto: CreateBoardDto, userId: number) {
     return 'This action adds a new board';
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} board`;
   }
 
-  update(id: number, updateBoardDto: UpdateBoardDto) {
+  async update(
+    id: number,
+    updateBoardDto: UpdateBoardDto,
+    userId: number,
+    role: Role,
+  ) {
     return `This action updates a #${id} board`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} board`;
+  async remove(id: number, userId: number, role: Role) {
+    const board = await this.boardRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        user: true,
+      },
+    });
+    if (!board) {
+      throw new BadRequestException('존재하지 않는 글입니다.');
+    }
+
+    if (board.user.id === userId || role === Role.ADMIN) {
+      return this.boardRepository.delete(id);
+    }
+
+    throw new UnauthorizedException('삭제 권한이 없습니다.');
   }
 }
