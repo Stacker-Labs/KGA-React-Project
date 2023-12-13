d19d387c4773be45fc2dab5466a850e88840d252
sangheun969 <sang969@naver.com>
2023-12-13 12:06:45 +0900

    fix: tailWind

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-
+import "./main.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
