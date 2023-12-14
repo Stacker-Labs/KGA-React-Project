a448dbcb569bdb9a2e99887f0d00ae7c558ffbd6
Eunjae Lee <vanitasium@gmail.com>
2023-12-14 13:24:34 +0900

    feat: social login access token recoil atoms에 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Login from "./pages/auth/Login";
 import Admin from "./pages/Admin";
 import Signup from "./pages/auth/Signup";
 import CssBaseline from "@mui/material/CssBaseline";
+import Github from "./pages/auth/Github";
+import Google from "./pages/auth/Google";
+import Kakao from "./pages/auth/Kakao";
 
 function App() {
   return (
@@ -17,9 +20,15 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/boards" element={<Board />} />
           <Route path="/boards/:id" element={<View />} />
+          <Route path="/boards/:id/edit" />
           <Route path="/users" element={<User />} />
-          <Route path="/auth" element={<Login />} />
-          <Route path="/auth/register" element={<Signup />} />
+          <Route path="/auth">
+            <Route index element={<Login />} />
+            <Route path="github/callback" element={<Github />} />
+            <Route path="google/callback" element={<Google />} />
+            <Route path="kakao/callback" element={<Kakao />} />
+            <Route path="register" element={<Signup />} />
+          </Route>
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </div>
---
+++ frontend/src/pages/auth/Google.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../recoil/User";
+
+export default () => {
+  const setUser = useSetRecoilState(userState);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const code = window.location.hash.substring(1).split("&")[0].split("=")[1];
+    const getAccessToken = async () => {
+      if (code) {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_SERVER}/auth/google/callback`,
+          {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              code,
+            }),
+          }
+        );
+        const result = await response.json();
+        setUser(result);
+        console.log(result);
+      }
+    };
+    getAccessToken();
+    navigate("/");
+  }, []);
+
+  return <div>Login to Google...</div>;
+};
