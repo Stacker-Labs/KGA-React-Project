6961e3ade0d562cef35293fac7fceae7982d5211
subin <github@subin.kr>
2023-12-21 13:58:10 +0900

    feat: get login user

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { usersProviders } from '../common/mock/provider/user.provider';
 
@@ -14,7 +15,15 @@ describe('UsersController', () => {
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('Get Login User', () => {
+    it('Return value is accessToken and user', async () => {
+      const result = await controller.getLoginUser('username');
+      const keys = Object.keys(result).sort();
+      expect(keys).toEqual(['accessToken', 'user']);
+    });
+    it('Throw error if not exist username', async () => {
+      const getLoginUser = controller.getLoginUser('username00');
+      await expect(getLoginUser).rejects.toThrow(BadRequestException);
+    });
   });
 });
