db65ff164785bae9b208a29ad3a1b18a23173d3b
Eunjae Lee <vanitasium@gmail.com>
2023-12-22 18:26:48 +0900

    fix: recoil-persist added

--- frontend/src/recoil/userState.js
+++ frontend/src/recoil/userState.js
@@ -1,4 +1,7 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const userState = atom({
   key: "userState",
@@ -6,4 +9,5 @@ export const userState = atom({
     message: "",
     user: "",
   },
+  effects_UNSTABLE: [persistAtom],
 });
