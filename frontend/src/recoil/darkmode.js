9b1c3c0b271b785358c4dbdd3d67ff1a4a3955f0
Eunjae Lee <vanitasium@gmail.com>
2023-12-22 18:43:07 +0900

    fix: darkmode persists

--- frontend/src/recoil/darkmode.js
+++ frontend/src/recoil/darkmode.js
@@ -1,6 +1,8 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export const darkModeState = atom({
   key: "darkModeState",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
