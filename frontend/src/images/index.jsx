1048fa1b772996cfd311a35b54c29ffe00adc934
Eunjae Lee <vanitasium@gmail.com>
2023-12-25 19:32:13 +0900

    fix: header default image added:

--- frontend/src/images/index.jsx
+++ frontend/src/images/index.jsx
@@ -4,12 +4,14 @@ import Github_icon from "./github.png";
 import Notion_icon from "./notion.png";
 import Js_icon from "./js_icon.png";
 import Notion_white from "./notion-logo-white.png";
+import No_Profile from "./blank-profile-picture.webp";
 
 export {
-  Search_icon,
-  TempUserImg,
-  Github_icon,
-  Notion_icon,
-  Js_icon,
-  Notion_white,
+    Search_icon,
+    TempUserImg,
+    Github_icon,
+    Notion_icon,
+    Js_icon,
+    Notion_white,
+    No_Profile,
 };
