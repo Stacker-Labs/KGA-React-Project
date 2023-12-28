14a7e09bf59ca3c11763b51d73972c9097f06b6f
sangheun969 <sang969@naver.com>
2023-12-28 12:44:26 +0900

    fix pagination Comment

--- frontend/src/pages/boards/BoardCom/ViewAtoms/CommentsList.jsx
+++ frontend/src/pages/boards/BoardCom/ViewAtoms/CommentsList.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Comment from "./Comment";
-// const Token = process.env.REACT_APP_TOKEN;
-//   Authorization: `Bearer ${Token}`,
+
 const CommentList = ({ id, commentList, setCommentList, page }) => {
+  const [commentList, setCommentList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasPreviousPage, setHasPreviousPage] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_SERVER}/boards/${id}/${currentPage}`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+
+        const result = await response.json();
+        setCommentList(result.boardComments[0]);
+        setHasPreviousPage(currentPage > 1);
+        setHasNextPage(result.nextPage);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+
+    fetchComments();
+  }, [id, currentPage]);
+
+  const loadPreviousPage = () => {
+    if (hasPreviousPage) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const loadNextPage = () => {
+    if (hasNextPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
   return (
     <div className="flex flex-col justify-center">
       {commentList?.map((comment, idx) => (
@@ -13,6 +50,17 @@ const CommentList = ({ id, commentList, setCommentList, page }) => {
           setCommentList={setCommentList}
         />
       ))}
+      <div className="p=[10px] w-[100%] flex flex-row justify-between">
+        <button onClick={loadPreviousPage} disabled={!hasPreviousPage}>
+          Previous
+        </button>
+        <div>
+          Page: {currentPage} / {totalPages}
+        </div>
+        <button onClick={loadNextPage} disabled={!hasNextPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
