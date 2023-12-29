e12fcb8e9aa2da29bcd859ee12d1373391f79490
Eunjae Lee <vanitasium@gmail.com>
2023-12-29 16:37:20 +0900

    add: 404 page added

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import { Not_Found } from "../images";
 
 const NotFound = () => {
-  return <img src={Not_Found} />;
+  return (
+    <div
+      style={{ width: "inherit", background: "white", position: "relative" }}
+    >
+      <img src={Not_Found} style={{ margin: "auto", marginTop: "3rem" }} />
+      <div
+        style={{
+          width: "100vw",
+          height: "80px",
+          position: "absolute",
+          background: "white",
+          bottom: "0px",
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default NotFound;
