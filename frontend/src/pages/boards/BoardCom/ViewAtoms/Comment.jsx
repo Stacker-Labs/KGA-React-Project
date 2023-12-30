75b8040a29edcdc1c3f0c453769379177985fbd3
sangheun969 <sang969@naver.com>
2023-12-30 15:59:35 +0900

    comment

--- frontend/src/pages/boards/BoardCom/ViewAtoms/Comment.jsx
+++ frontend/src/pages/boards/BoardCom/ViewAtoms/Comment.jsx
@@ -7,20 +7,14 @@ import ReplyCommentList from "./ReplyCommentList";
 
 // const Token = process.env.REACT_APP_TOKEN;
 //    Authorization: `Bearer ${Token}`,
-const Comment = ({
-  comment,
-  id,
-  setCommentList,
-  replyCommentList,
-  setReplyCommentList,
-}) => {
+const Comment = ({ comment, id, setCommentList }) => {
   const [updateMode, setUpdateMode] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
   const [replyMode, setReplyMode] = useState(false);
   const [replyContent, setReplyContent] = useState("");
   const [replyCommentList, setReplyCommentList] = useState(comment.comments);
 
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   const userInfo = useRecoilValue(userState);
   const userId = userInfo?.user?.id;
   // const params = useParams();
