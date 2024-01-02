2a8b6db60f1818b9ed75397249240a8d06a08d23
sangheun969 <sang969@naver.com>
2024-01-02 13:12:08 +0900

    fix Board

--- frontend/src/pages/boards/BoardCom/View.jsx
+++ frontend/src/pages/boards/BoardCom/View.jsx
@@ -146,16 +146,16 @@ const View = () => {
         // darkMode ? "dark" : ""
       )}
     >
+      <div className="sticky top-0 flex justify-end">
+        <HandleScroll
+          userId={userId}
+          postId={params.id}
+          isLiked={isLiked}
+          setIsLiked={setIsLiked}
+        />
+      </div>
       <div className="w-full flex flex-row justify-between items-center">
         <div className="text-4xl">{viewContent.title}</div>
-        <div className="sticky top-0">
-          <HandleScroll
-            userId={userId}
-            postId={params.id}
-            isLiked={isLiked}
-            setIsLiked={setIsLiked}
-          />
-        </div>
       </div>
 
       <div className="flex flex-row gap-5 text-xl">
